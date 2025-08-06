aad25b47e9d3690c1c47e6ca85bdd74c248beb41
Ali Khatami <aliikhatami94@gmail.com>
2025-08-06 19:37:05 -0400

    fix: use named exports in src/index.ts for npm consumers

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './components/ApiFrameworksDoc';
+export * from './components/ApiDocsSidebar';
+export * from './components/ComponentsSection';
+export * from './components/EndpointSection';
+export * from './components/HttpMethodBadge';
+export * from './components/ui/button';
