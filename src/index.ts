427dec8011f1abaa1bcb2173eb59a8d15269dafe
Ali Khatami <aliikhatami94@gmail.com>
2025-08-06 20:39:37 -0400

    chore: republish and update npm package

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,3 @@ export * from './components/ComponentsSection';
 export * from './components/EndpointSection';
 export * from './components/HttpMethodBadge';
 export { Button } from "./components/ui/button";
-
